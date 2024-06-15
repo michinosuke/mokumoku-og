1198e9dbba1eeb98a4a7463665a15284bdaa965a
Michinosuke <michinosuke.info@gmail.com>
2024-06-16 03:28:24 +0900

    fix

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -4,6 +4,7 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 import { nanoid } from "nanoid";
 import { useState } from "react";
+import Image from "next/image";
 
 const client = new S3Client({
   region: "ap-northeast-1",
@@ -11,45 +12,77 @@ const client = new S3Client({
     accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY!,
     secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY!,
   },
-  endpoint: "https://d35yi2p0ou13gt.cloudfront.net",
 });
 
-export default () => {
-  const [file, setFile] = useState<File | null>(null);
+export default function Upload() {
+  const [key, setKey] = useState<string | null>(null);
+  const [copied, setCopied] = useState(false);
 
-  const upload = async () => {
+  const url = () =>
+    `https://${process.env.NEXT_PUBLIC_S3_BUCKET}.s3.ap-northeast-1.amazonaws.com/${key}`;
+
+  const upload = async (file: File) => {
     if (!file) return;
     const key = nanoid(6);
     const command = new PutObjectCommand({
       Bucket: process.env.NEXT_PUBLIC_S3_BUCKET!,
       Key: key,
-      Body: file.stream(),
+      Body: file as any,
     });
-    const result = await client.send(command);
-    console.log(result);
+
+    try {
+      const result = await client.send(command);
+      if (result.$metadata.httpStatusCode === 200) {
+        setKey(key);
+      }
+    } catch (e: any) {
+      alert(e.message);
+    }
   };
+
+  if (key) {
+    return (
+      <main className="px-10 py-10">
+        <div className="relative">
+          <Image
+            src={url()}
+            width={200}
+            height={200}
+            alt="アップロードした画像"
+            className="h-full"
+          />
+        </div>
+        <p className="mt-10">{url()}</p>
+        <p className="mt-10 flex gap-5 items-center">
+          <button
+            className="py-2 px-3 border rounded"
+            onClick={() => {
+              navigator.clipboard.writeText(url());
+              setCopied(true);
+            }}
+          >
+            URL をコピー
+          </button>
+          {copied && <span>コピーしました！</span>}
+        </p>
+      </main>
+    );
+  }
 
   return (
     <main className="py-10 px-10">
-      <div>
+      <h1 className="text-xl">Upload Image</h1>
+      <div className="mt-10">
         <input
           type="file"
           accept=".jpg,.png"
           onChange={(e) => {
             const file = e.currentTarget.files?.[0];
             if (!file) return;
-            setFile(file);
+            upload(file);
           }}
         />
       </div>
-      <div>
-        <button
-          onClick={() => upload()}
-          className="px-3 py-2 border rounded mt-10"
-        >
-          Upload
-        </button>
-      </div>
     </main>
   );
-};
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: `${process.env.NEXT_PUBLIC_S3_BUCKET}.s3.ap-northeast-1.amazonaws.com`,
+      },
+    ],
+  },
+};
 
 export default nextConfig;
