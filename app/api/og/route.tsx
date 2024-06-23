25978cf014754d4a0256cbec12d5fb1a609e9e8f
Michinosuke <michinosuke.info@gmail.com>
2024-06-23 17:09:00 +0900

    change color

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -4,6 +4,9 @@ import { BgSvg03 } from "@/components/bg-svg-03";
 import { ImageResponse } from "next/og";
 import { join } from "path";
 import { readFile } from "fs/promises";
+import { Icon01 } from "@/components/icon-01";
+import { Icon02 } from "@/components/icon-02";
+import { Icon03 } from "@/components/icon-03";
 // App router includes @vercel/og.
 // No need to install it.
 
@@ -24,6 +27,12 @@ export async function GET(request: Request) {
     return BgSvg03;
   })();
 
+  const Icon = (() => {
+    if (Number(day) <= 10) return Icon01;
+    if (Number(day) <= 20) return Icon02;
+    return Icon03;
+  })();
+
   return new ImageResponse(
     (
       <div
@@ -37,7 +46,13 @@ export async function GET(request: Request) {
           position: "relative",
         }}
       >
-        <BG style={{}} />
+        <BG
+          style={{
+            position: "absolute",
+            left: 0,
+            top: 0,
+          }}
+        />
 
         <div
           style={{
@@ -50,47 +65,7 @@ export async function GET(request: Request) {
             zIndex: 1000,
           }}
         >
-          <svg
-            id="ロゴ"
-            width="74"
-            height="74"
-            viewBox="0 0 74 74"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g clip-path="url(#clip0_126_2067)">
-              <path d="M1 1L19 8H55L73 1V73L55 65.5H19L1 73V1Z" fill="white" />
-              <path
-                d="M72.596 0.264366L54.8024 7.38149H19.2507L1.45715 0.264366C0.812418 0.0229579 0.226562 0.51019 0.226562 1.09899V72.9046C0.226562 73.492 0.812418 73.9807 1.45715 73.7392L19.2507 66.6221H54.801L72.5945 73.7392C73.2392 73.9807 73.8251 73.492 73.8251 72.9046V1.09899C73.8251 0.511662 73.2392 0.0229579 72.5945 0.264366H72.596ZM18.1776 65.1163L2.02093 71.5783V2.42379L18.1776 8.88587V65.1163ZM54.0797 64.8248H19.972V9.17585H54.0797V64.8248ZM72.0307 71.5783L55.8741 65.1163V8.88587L72.0307 2.42379V71.5783Z"
-                fill="#051B2E"
-              />
-              <path
-                d="M19.8516 8.78516H54.1982V65.2118H19.8516V8.78516Z"
-                fill="url(#paint0_radial_126_2067)"
-              />
-            </g>
-            <defs>
-              <radialGradient
-                id="paint0_radial_126_2067"
-                cx="0"
-                cy="0"
-                r="1"
-                gradientUnits="userSpaceOnUse"
-                gradientTransform="translate(37.0249 36.9985) rotate(90) scale(28.2133 17.1733)"
-              >
-                <stop stop-color="#FFF2AC" />
-                <stop offset="1" stop-color="#F5D421" />
-              </radialGradient>
-              <clipPath id="clip0_126_2067">
-                <rect
-                  width="73.6"
-                  height="73.6"
-                  fill="white"
-                  transform="translate(0.226562 0.199219)"
-                />
-              </clipPath>
-            </defs>
-          </svg>
+          {<Icon />}
           <div
             style={{
               display: "flex",
