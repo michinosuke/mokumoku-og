fe1a14a1330149c1e0765b184dcbd41a70ed9112
Michinosuke <michinosuke.info@gmail.com>
2024-10-22 03:13:41 +0900

    依頼対応

--- app/iret-media/page.tsx
+++ app/iret-media/page.tsx
@@ -72,149 +72,159 @@ export default function IretMedia() {
   console.log(templates);
 
   return (
-    <main className="min-h-screen relative bg-[#e9ecee]">
-      <header className="h-16 w-full bg-white flex items-center justify-center">
-        <img src="/iret-media-logo.svg" alt="iret media" className="h-8" />
-      </header>
-      <div className="flex flex-col sm:flex-row gap-8 mt-5 px-10">
-        <div className="flex flex-col flex-1">
-          <span className="whitespace-nowrap font-bold border-b-2 pb-1 border-iret-media-theme text-slate-600">
-            タイトル
-          </span>
-          <textarea
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className=" bg-white w-full rounded-lg px-5 py-2 mt-3 font-bold focus:outline-iret-media-theme"
-            placeholder="タイトルを入力してください。"
-            rows={6}
-          />
-          <span className="whitespace-nowrap font-bold border-b-2 pb-1 border-iret-media-theme text-slate-600 mt-5">
-            テンプレート（{templates.length}種）
-          </span>
-          <ul className="flex gap-3 mt-5 overflow-x-scroll items-center">
-            {templates.map((template, i) => (
-              <li
-                key={i}
-                className={`${
-                  selectedTemplate.url === template.url
-                    ? "border-2 border-iret-media-theme"
-                    : "opacity-60"
-                } shrink-0`}
-                onClick={() => setSelectedTemplate(template)}
-              >
-                <img
-                  src={template.url}
-                  className={`${
-                    selectedTemplate.url === template.url ? "h-32" : "h-28"
-                  }`}
-                />
-              </li>
-            ))}
-          </ul>
-          <span className="whitespace-nowrap font-bold border-b-2 pb-1 border-iret-media-theme text-slate-600 mt-8">
-            フォントサイズ
-          </span>
-          <div>
-            <input
-              value={fontSize}
-              type="number"
-              onChange={(e) => setFontSize(parseInt(e.target.value, 10))}
-              className="bg-white px-5 py-2 rounded-lg mt-3 w-24 focus:outline-iret-media-theme"
+    <>
+      <main className="min-h-[95vh] relative bg-[#e9ecee]">
+        <header className="h-16 w-full bg-white flex items-center justify-center">
+          <img src="/iret-media-logo.svg" alt="iret media" className="h-8" />
+        </header>
+        <div className="flex flex-col sm:flex-row gap-8 mt-5 px-10">
+          <div className="flex flex-col flex-1">
+            <span className="whitespace-nowrap font-bold border-b-2 pb-1 border-iret-media-theme text-slate-600">
+              タイトル
+            </span>
+            <textarea
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className=" bg-white w-full rounded-lg px-5 py-2 mt-3 font-bold focus:outline-iret-media-theme text-slate-700"
+              placeholder="タイトルを入力してください。"
+              rows={6}
             />
-            <span className="pl-3 text-slate-700">px</span>
-          </div>
-          <span className="whitespace-nowrap font-bold border-b-2 pb-1 border-iret-media-theme text-slate-600 mt-8">
-            揃え方
-          </span>
-          <div className="flex mt-3">
-            <button
-              onClick={(e) => setTitleAlign("left")}
-              className={`px-5 py-2 whitespace-nowrap rounded-lg ${
-                titleAlign === "left" ? "bg-slate-700" : ""
-              }`}
-            >
-              <div className="w-8 h-8 flex flex-col justify-between">
-                <div
+            <span className="whitespace-nowrap font-bold border-b-2 pb-1 border-iret-media-theme text-slate-600 mt-5">
+              テンプレート（{templates.length}種）
+            </span>
+            <p className="text-xs mt-3 text-slate-500">
+              ※{templates.length}
+              種類ありますのでお好みのデザインを選択してください。横にスライドすると全てのデザインを確認できます。
+            </p>
+            <ul className="flex gap-3 mt-5 overflow-x-scroll items-center">
+              {templates.map((template, i) => (
+                <li
+                  key={i}
                   className={`${
-                    titleAlign === "left" ? "bg-white" : "bg-black"
-                  } h-1 rounded-full`}
-                ></div>
-                <div
-                  className={`${
-                    titleAlign === "left" ? "bg-white" : "bg-black"
-                  } h-1 rounded-full w-4/5`}
-                ></div>
-                <div
-                  className={`${
-                    titleAlign === "left" ? "bg-white" : "bg-black"
-                  } h-1 rounded-full`}
-                ></div>
-                <div
-                  className={`${
-                    titleAlign === "left" ? "bg-white" : "bg-black"
-                  } h-1 rounded-full w-4/5`}
-                ></div>
-              </div>
-            </button>
-            <button
-              onClick={(e) => setTitleAlign("center")}
-              className={`px-5 py-2 whitespace-nowrap rounded-lg ${
-                titleAlign === "center" ? "bg-slate-700" : ""
-              }`}
-            >
-              <div className="w-8 h-8 flex flex-col justify-between items-center">
-                <div
-                  className={`${
-                    titleAlign === "center" ? "bg-white" : "bg-black"
-                  } h-1 rounded-full w-full`}
-                ></div>
-                <div
-                  className={`${
-                    titleAlign === "center" ? "bg-white" : "bg-black"
-                  } h-1 rounded-full w-4/5`}
-                ></div>
-                <div
-                  className={`${
-                    titleAlign === "center" ? "bg-white" : "bg-black"
-                  } h-1 rounded-full w-full`}
-                ></div>
-                <div
-                  className={`${
-                    titleAlign === "center" ? "bg-white" : "bg-black"
-                  } h-1 rounded-full w-4/5`}
-                ></div>
-              </div>
-            </button>
-          </div>
-        </div>
-        <div className="flex-1 flex gap-5 flex-col items-center mt-3">
-          <div className="flex flex-col gap-5 justify-center items-center min-h-20">
-            {objectUrl && <img src={objectUrl} className="shadow-xl" />}
-            {isLoading && (
-              <Loading
-                type="spokes"
-                className="absolute"
-                color="#58db15"
-                width={70}
+                    selectedTemplate.url === template.url
+                      ? "border-2 border-iret-media-theme"
+                      : "opacity-60"
+                  } shrink-0`}
+                  onClick={() => setSelectedTemplate(template)}
+                >
+                  <img
+                    src={template.url}
+                    className={`${
+                      selectedTemplate.url === template.url ? "h-32" : "h-28"
+                    }`}
+                  />
+                </li>
+              ))}
+            </ul>
+            <span className="whitespace-nowrap font-bold border-b-2 pb-1 border-iret-media-theme text-slate-600 mt-8">
+              フォントサイズ
+            </span>
+            <div>
+              <input
+                value={fontSize}
+                type="number"
+                onChange={(e) => setFontSize(parseInt(e.target.value, 10))}
+                className="bg-white px-5 py-2 rounded-lg mt-3 w-24 focus:outline-iret-media-theme text-slate-700"
               />
-            )}
-          </div>
-          {objectUrl && (
-            <a
-              href={objectUrl}
-              download={`iret-media-${format(new Date(), "yyyyMMdd-HHmmss")}`}
-            >
-              <button className="bg-slate-700 text-white px-3 py-2 rounded-lg font-bold">
-                ダウンロード
+              <span className="pl-3 text-slate-700">px</span>
+            </div>
+            <span className="whitespace-nowrap font-bold border-b-2 pb-1 border-iret-media-theme text-slate-600 mt-8">
+              揃え方
+            </span>
+            <div className="flex mt-3">
+              <button
+                onClick={(e) => setTitleAlign("left")}
+                className={`px-5 py-2 whitespace-nowrap rounded-lg ${
+                  titleAlign === "left" ? "bg-slate-700" : ""
+                }`}
+              >
+                <div className="w-8 h-8 flex flex-col justify-between">
+                  <div
+                    className={`${
+                      titleAlign === "left" ? "bg-white" : "bg-black"
+                    } h-1 rounded-full`}
+                  ></div>
+                  <div
+                    className={`${
+                      titleAlign === "left" ? "bg-white" : "bg-black"
+                    } h-1 rounded-full w-4/5`}
+                  ></div>
+                  <div
+                    className={`${
+                      titleAlign === "left" ? "bg-white" : "bg-black"
+                    } h-1 rounded-full`}
+                  ></div>
+                  <div
+                    className={`${
+                      titleAlign === "left" ? "bg-white" : "bg-black"
+                    } h-1 rounded-full w-4/5`}
+                  ></div>
+                </div>
               </button>
-            </a>
-          )}
+              <button
+                onClick={(e) => setTitleAlign("center")}
+                className={`px-5 py-2 whitespace-nowrap rounded-lg ${
+                  titleAlign === "center" ? "bg-slate-700" : ""
+                }`}
+              >
+                <div className="w-8 h-8 flex flex-col justify-between items-center">
+                  <div
+                    className={`${
+                      titleAlign === "center" ? "bg-white" : "bg-black"
+                    } h-1 rounded-full w-full`}
+                  ></div>
+                  <div
+                    className={`${
+                      titleAlign === "center" ? "bg-white" : "bg-black"
+                    } h-1 rounded-full w-4/5`}
+                  ></div>
+                  <div
+                    className={`${
+                      titleAlign === "center" ? "bg-white" : "bg-black"
+                    } h-1 rounded-full w-full`}
+                  ></div>
+                  <div
+                    className={`${
+                      titleAlign === "center" ? "bg-white" : "bg-black"
+                    } h-1 rounded-full w-4/5`}
+                  ></div>
+                </div>
+              </button>
+            </div>
+          </div>
+          <div className="flex-1 flex gap-5 flex-col items-center mt-3">
+            <div className="flex flex-col gap-5 justify-center items-center min-h-20">
+              {objectUrl && <img src={objectUrl} className="shadow-xl" />}
+              {isLoading && (
+                <Loading
+                  type="spokes"
+                  className="absolute"
+                  color="#58db15"
+                  width={70}
+                />
+              )}
+            </div>
+            {objectUrl && (
+              <a
+                href={objectUrl}
+                download={`iret-media-${format(new Date(), "yyyyMMdd-HHmmss")}`}
+              >
+                <button className="bg-slate-700 text-white px-3 py-2 rounded-lg font-bold">
+                  ダウンロード
+                </button>
+              </a>
+            )}
+            <div className="mt-14 px-5 py-2 text-slate-700 border-4 border-iret-media-theme border-dotted rounded-lg">
+              <p>
+                ※画像が設定されていない場合は、マーケGにて作成させていただきます。
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="h-16" />
-      <footer className="bottom-5 text-slate-400 text-center py-4">
+      </main>
+      <footer className="text-slate-400 text-center py-4 bg-[#e9ecee]">
         ©︎ Michinosuke
       </footer>
-    </main>
+    </>
   );
 }
